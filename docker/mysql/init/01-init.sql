7f1b9be5b7ca778bc0b49cb08d5017aadf8af79d
Lucas <lucas.spingles@hotmail.com>
2026-04-15 14:14:24 -0300

    Alterações após revisão de modelagem 15.04.2026

--- docker/mysql/init/01-init.sql
+++ docker/mysql/init/01-init.sql
@@ -17,3 +17,48 @@ CREATE TABLE IF NOT EXISTS posts (
 );
 INSERT IGNORE INTO users (name, email) VALUES ('Alice', 'alice@example.com');
 INSERT IGNORE INTO posts (title, content, user_id) VALUES ('Primeira Postagem', 'Conteúdo da primeira postagem.', 1);
+
+CREATE DATABASE MedAlerta;
+USE MedAlerta;
+
+
+
+CREATE TABLE Usuario (
+	idUsuario int primary key not null,
+	nome varchar(15) not null,
+	telefone int (14) not null,
+	email varchar (25) not null,
+	endereço varchar (20),
+	endereçoRua varchar (20),
+	endereçoNumero varchar (20),
+	endereçoComp varchar (20),
+	endereçoBairro varchar (20),
+	endereçoCEP varchar (20),
+	endereçoCidade varchar (20),
+	endereçoEstado varchar (20)
+);
+
+CREATE TABLE usuarioMedicamento(
+	IdUsuario int not null,
+	IdMedicamento int not null,
+	horarioUso time not null,
+	frequenciaUso varchar(50),
+	dosagem varchar(50),
+	dataHOrarioAlerta datetime not null,
+	statusAlerta enum('emitido', 'Nao emitido');
+	dataHorarioConsumo datetime,
+	confirmacaoConsumo enum('sim', 'nao') not null,
+	primary key (IdUsuario, IdMedcimaento),
+	foreign key (IdUsuario) references (IdUsuario),
+	foreign key (IdMedcimaento) references Medicamento (IdMedicamento)
+);
+
+CREATE TABLE Medicamento (
+	IdMedcimaento int primary key,
+	nomeComercial varchar(100),
+	nomeGenerico varchar(100),
+	quantidade enum('unidade', 'ml'),
+	formaUso varchar(100),
+	observacao varchar(200)	
+);
+
