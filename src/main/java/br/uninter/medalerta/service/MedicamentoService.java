c49a76e9457da146eda9c27b08060370d95af7d7
Gleyce Pires <gleyce-p@hotmail.com>
2026-04-16 14:02:36 -0300

    cria o servico de medicamento

--- src/main/java/br/uninter/medalerta/service/MedicamentoService.java
+++ src/main/java/br/uninter/medalerta/service/MedicamentoService.java
@@ -2,46 +2,57 @@ package br.uninter.medalerta.service;
 
 import br.uninter.medalerta.model.Medicamento;
 import br.uninter.medalerta.repository.MedicamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MedicamentoService {
 
-    private final MedicamentoRepository repository;
+    private final MedicamentoRepository medicamentoRepository;
 
-    public MedicamentoService(MedicamentoRepository repository) {
-        this.repository = repository;
+    public MedicamentoService(MedicamentoRepository medicamentoRepository) {
+        this.medicamentoRepository = medicamentoRepository;
     }
 
-    public Medicamento salvar(Medicamento Medicamento) {
-        return repository.save(Medicamento);
+    public Medicamento salvar(Medicamento medicamento) {
+        return medicamentoRepository.save(medicamento);
     }
 
     public List<Medicamento> listarTodos() {
-        return repository.findAll();
+        return medicamentoRepository.findAll();
     }
 
-    public Medicamento buscarPorId(Integer id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Medicamento não encontrado: " + id));
+    public Optional<Medicamento> buscarPorId(Integer id) {
+        return medicamentoRepository.findById(id);
     }
 
-    public Medicamento atualizar(Integer id, Medicamento novoMedicamento) {
-        Medicamento existente = buscarPorId(id);
+    public List<Medicamento> buscarPorNomeComercial(String nome) {
+        return medicamentoRepository.findByNomeComercialContainingIgnoreCase(nome);
+    }
 
-        existente.setNomeComercial(novoMedicamento.getNomeComercial());
-        existente.setNomeGenerico(novoMedicamento.getNomeGenerico());
-        existente.setQuantidade(novoMedicamento.getQuantidade());
-        existente.setFormaUso(novoMedicamento.getFormaUso());
-        existente.setObservacao(novoMedicamento.getObservacao());
+    public List<Medicamento> buscarPorNomeGenerico(String nome) {
+        return medicamentoRepository.findByNomeGenericoContainingIgnoreCase(nome);
+    }
 
-        return repository.save(existente);
+    public Medicamento atualizar(Integer id, Medicamento dadosAtualizados) {
+        return medicamentoRepository.findById(id).map(medicamento -> {
+            medicamento.setNomeComercial(dadosAtualizados.getNomeComercial());
+            medicamento.setNomeGenerico(dadosAtualizados.getNomeGenerico());
+            medicamento.setQuantidade(dadosAtualizados.getQuantidade());
+            medicamento.setFormaUso(dadosAtualizados.getFormaUso());
+            medicamento.setObservacao(dadosAtualizados.getObservacao());
+            return medicamentoRepository.save(medicamento);
+        }).orElseThrow(() -> new RuntimeException("Medicamento não encontrado com id: " + id));
     }
 
     public void deletar(Integer id) {
-        Medicamento Medicamento = buscarPorId(id);
-        repository.delete(Medicamento);
+        Optional<Medicamento> medicamento = medicamentoRepository.findById(id);
+        if (medicamento.isPresent()) {
+            medicamentoRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Medicamento não encontrado com id: " + id);
+        }
     }
 }
