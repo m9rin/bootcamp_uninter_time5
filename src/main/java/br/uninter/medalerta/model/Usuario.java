22d473bf987454dadbea1744fdd034310cb40d4e
Gabriel Marin <marinjhow@gmail.com>
2026-04-16 12:25:14 -0300

    cria as entidades usuario e endereco

--- src/main/java/br/uninter/medalerta/model/Usuario.java
+++ src/main/java/br/uninter/medalerta/model/Usuario.java
@@ -1,7 +1,6 @@
 package br.uninter.medalerta.model;
 
 import jakarta.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -13,136 +12,62 @@ public class Usuario {
     @Column(name = "idUsuario")
     private Integer idUsuario;
 
-    @Column(nullable = false, length = 100)
+    @Column(name = "nome", nullable = false, length = 100)
     private String nome;
 
-    @Column(nullable = false, length = 20)
+    @Column(name = "telefone", nullable = false, length = 20)
     private String telefone;
 
-    @Column(nullable = false, length = 100)
+    @Column(name = "email", nullable = false, length = 100)
     private String email;
 
-    @Column(length = 100)
-    private String enderecoRua;
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Endereco> enderecos;
 
-    private Integer enderecoNumero;
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tratamento> tratamentos;
 
-    @Column(length = 50)
-    private String enderecoComplemento;
-
-    @Column(length = 50)
-    private String enderecoBairro;
-
-    @Column(length = 10)
-    private String enderecoCEP;
-
-    @Column(length = 50)
-    private String enderecoCidade;
-
-    @Column(length = 2)
-    private String enderecoEstado;
-
-    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = false)
-    private List<UsuarioMedicamento> usuarioMedicamentos = new ArrayList<>();
-
-    public Usuario() {
+    public Integer getIdUsuario() { 
+        return idUsuario; 
+    }
+    public void setIdUsuario(Integer idUsuario) { 
+        this.idUsuario = idUsuario; 
     }
 
-    public Integer getIdUsuario() {
-        return idUsuario;
+    public String getNome() { 
+        return nome; 
+    }
+    public void setNome(String nome) { 
+        this.nome = nome; 
     }
 
-    public void setIdUsuario(Integer idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTelefone() {
+    public String getTelefone() { 
         return telefone;
-    }
-
-    public void setTelefone(String telefone) {
+     }
+    public void setTelefone(String telefone) { 
         this.telefone = telefone;
-    }
+     }
 
-    public String getEmail() {
+    public String getEmail() { 
         return email;
-    }
-
-    public void setEmail(String email) {
+     }
+    public void setEmail(String email) { 
         this.email = email;
-    }
+     }
 
-    public String getEnderecoRua() {
-        return enderecoRua;
-    }
+    public List<Endereco> getEnderecos() { 
+        return enderecos;
+     }
+    public void setEnderecos(List<Endereco> enderecos) { 
+        this.enderecos = enderecos;
+     }
 
-    public void setEnderecoRua(String enderecoRua) {
-        this.enderecoRua = enderecoRua;
-    }
-
-    public Integer getEnderecoNumero() {
-        return enderecoNumero;
-    }
-
-    public void setEnderecoNumero(Integer enderecoNumero) {
-        this.enderecoNumero = enderecoNumero;
-    }
-
-    public String getEnderecoComplemento() {
-        return enderecoComplemento;
-    }
-
-    public void setEnderecoComplemento(String enderecoComplemento) {
-        this.enderecoComplemento = enderecoComplemento;
-    }
-
-    public String getEnderecoBairro() {
-        return enderecoBairro;
-    }
-
-    public void setEnderecoBairro(String enderecoBairro) {
-        this.enderecoBairro = enderecoBairro;
-    }
-
-    public String getEnderecoCEP() {
-        return enderecoCEP;
-    }
-
-    public void setEnderecoCEP(String enderecoCEP) {
-        this.enderecoCEP = enderecoCEP;
-    }
-
-    public String getEnderecoCidade() {
-        return enderecoCidade;
-    }
-
-    public void setEnderecoCidade(String enderecoCidade) {
-        this.enderecoCidade = enderecoCidade;
-    }
-
-    public String getEnderecoEstado() {
-        return enderecoEstado;
-    }
-
-    public void setEnderecoEstado(String enderecoEstado) {
-        this.enderecoEstado = enderecoEstado;
-    }
-
-    public List<UsuarioMedicamento> getUsuarioMedicamentos() {
-        return usuarioMedicamentos;
-    }
-
-    public void setUsuarioMedicamentos(List<UsuarioMedicamento> usuarioMedicamentos) {
-        this.usuarioMedicamentos = usuarioMedicamentos;
-    }
+    public List<Tratamento> getTratamentos() { 
+        return tratamentos;
+     }
+    public void setTratamentos(List<Tratamento> tratamentos) { 
+        this.tratamentos = tratamentos;
+     }
 
     @Override
     public String toString() {
@@ -151,13 +76,6 @@ public class Usuario {
                 ", nome='" + nome + '\'' +
                 ", telefone='" + telefone + '\'' +
                 ", email='" + email + '\'' +
-                ", enderecoRua='" + enderecoRua + '\'' +
-                ", enderecoNumero=" + enderecoNumero +
-                ", enderecoComplemento='" + enderecoComplemento + '\'' +
-                ", enderecoBairro='" + enderecoBairro + '\'' +
-                ", enderecoCEP='" + enderecoCEP + '\'' +
-                ", enderecoCidade='" + enderecoCidade + '\'' +
-                ", enderecoEstado='" + enderecoEstado + '\'' +
                 '}';
     }
 }
